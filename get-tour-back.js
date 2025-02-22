fc597cb64535f9cf5f8013c0fa694eda0c1b7d66
kiril.shevchenko <kiril.shevchenko@x5.ru>
2025-02-22 18:05:07 +0300

    refactor

--- get-tour-back.js
+++ get-tour-back.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT;
 
-app.use('/subscriptions', require('./routes/subscriptions'));
-app.use('/chat-subscriptions', require('./routes/chat-subscriptions'));
+app.use('/subscriptions', require('./src/routes/subscriptions'));
+app.use('/chat-subscriptions', require('./src/routes/chat-subscriptions'));
 
 app.listen(PORT, () => {
     console.log(`server is runnig in PORT: ${PORT}`);
