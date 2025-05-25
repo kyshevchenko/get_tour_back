1dab43ba79b840b482591bb2f41a916d6ea7e7f9
kiril.shevchenko <kiril.shevchenko@x5.ru>
2025-05-25 17:35:35 +0300

    configure private route

--- src/routes/private.js
+++ src/routes/private.js
@@ -7,7 +7,20 @@ router.get("/", async (req, res) => {
   try {
     const privateData = await Privat.findAll();
 
-    res.json(privateData);
+    const privateResult = privateData.reduce((result, item) => {
+      const pureId = BigInt(String(item.channelId).replace(/^-100/, ""));
+
+      // console.log("item.channelId): ", item.channelId); TODO: remove, used for debug
+      // console.log("ipureId: ", pureId);
+
+      result[pureId] = {
+        keywords: item.keywords.split(", "),
+        recipients: item.recipients.split(", ").map(Number),
+      };
+      return result;
+    }, {});
+
+    res.json(privateResult);
   } catch (error) {
     res.status(500).json({
       error: somethingWrongMessage,
